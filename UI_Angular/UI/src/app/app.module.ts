91aa7114ea9df5fdb323009f8f7549708375c545
Nikolay Neshev <nikkie@Nikolays-MacBook-Pro.local>
2021-06-13 16:47:43 +0300

    Update

--- UI_Angular/UI/src/app/app.module.ts
+++ UI_Angular/UI/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UI_Angular/UI/src/app/routing.module.ts
+++ UI_Angular/UI/src/app/routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_helpers';
 
 const ROUTES: Routes = [
+  {path: 'register', component: RegisterComponent}
   // Fill out different application routes here  
 ];
 
